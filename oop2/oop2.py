8d26d303f7f0e4d6247524c8ceda7e58ae9e7cfd
Kimokot <bkim19302@gmail.com>
2016-08-19 17:11:46 +0300

    Adds Console Application Support

--- oop2/oop2.py
+++ oop2/oop2.py
@@ -64,8 +64,57 @@ class NotesApplication(object):
             raise IndexError
 
 
-# note5=NotesApplication("Ken")
-# note5.create("Note A")
-# note5.list()
-# note5.edit(0, "")
-# note5.list()
+print("Enter a Command Number to Choose Action:")
+print '''
+    1. Create Note
+    2. List Notes
+    3. Get a Note
+    4. Search text from a Note
+    5. Delete a Note
+    6. Quit
+'''
+name = str(raw_input("Enter Author Name:"))
+if len(name) > 0 and type(name) == str:
+    author1 = NotesApplication(name)
+    while True:
+        command = raw_input("Enter Command Number: ")
+        
+        # if type(command) == int:
+        if command == '1':
+            note = str(raw_input("Enter Note: "))
+            if type(note) == str and len(note) > 0:
+                author1.create(note)
+        elif command == '2':
+            listing = author1.list()
+            if listing != "Listing Successful":
+                print(listing)
+        elif command == '6':
+            break
+        elif command == '3':
+            try:
+                note_id = int(raw_input("Enter List Index: "))
+                print(author1.get(note_id))
+            except IndexError:
+                print("The Note Id does not Exist")
+            except TypeError:
+                print("Please Enter a Number for index")
+        elif command == '4':
+            search_text=raw_input("Enter Search text to Search from Notes: ")
+            if len(search_text) > 0:
+                result = author1.search(search_text)
+                if result != "Found Search Text":
+                    print(result)
+        elif command == "5":
+            try:
+                index = int(raw_input("Enter Index of Note to Delete: "))
+                deletion=author1.delete(index)
+                print(deletion)
+            except TypeError:
+                print("Please Enter a Number")
+            except IndexError:
+                print("The Index Entered Does not Exist")
+        else:
+            print("Invalid Choice. Enter a number from 1 to 6")
+
+
+
